cfc55a13e7f11eedfc9055f4a222243789140dce
yoniedilzar <yoniedilzar@gmail.com>
2023-03-25 15:34:57 -0600

    crud modificado parte 2

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -3,13 +3,18 @@ import Swal from 'sweetalert2'
 
 const Formulario = () => {
 
-    const[persona,setPersona]=useState('')
+    const[codigo,setCodigo]=useState('')
     const[nombre,setNombre]=useState('')
     const[apellido,setApellido]=useState('')
     const[edad,setEdad]=useState('')
     const[correo,setCorreo]=useState('')
     const[lista,setLista]=useState([])
+    const[modoEdicion,setModoEdicion]=useState(false)
 
+
+    const generarId=()=>{
+        return Math.floor(Math.random()*100000000)
+    }
 
     const llenarCampos = (dato) => {
         Swal.fire({
@@ -20,8 +25,10 @@ const Formulario = () => {
     }
         
 
-    const guardarDatos = (e) => {
+    const agregarUsuario = (e) => {
+
         e.preventDefault() //evita que se recargue la pagina
+        
         if(nombre.trim()===''){
             llenarCampos('Nombre')
             return
@@ -40,12 +47,9 @@ const Formulario = () => {
         }
 
     //setPersona({nombre,apellido,edad,correo}) //agrega los datos al objeto persona
-        setLista([
-            ...lista,
-            {nombre,apellido,edad,correo}]) //agrega los datos al array lista
+       const usuario = {codigo:generarId(), nombre,apellido,edad,correo}
+         setLista([...lista,usuario])
 
-        //limpiar campos
-        e.target.reset()
         setNombre('')
         setApellido('')
         setEdad('')
@@ -61,7 +65,58 @@ const Formulario = () => {
 
     }
 
-    const eliminar = (index) => { //2 parametros, el primero es el item y el segundo es el index
+
+    const editar = (persona) => { // 1 parametro, el item
+        setCodigo(persona.codigo)
+        setNombre(persona.nombre)
+        setApellido(persona.apellido)
+        setEdad(persona.edad)
+        setCorreo(persona.correo)
+        setModoEdicion(true)
+
+    }
+    
+    const guardarCambios=(e)=>{
+        e.preventDefault()
+        if(nombre.trim()===''){
+            llenarCampos('Nombre')
+            return
+        }
+        if(apellido.trim()===''){
+            llenarCampos('Apellido')
+            return
+        }
+        if(edad.trim()===''){
+            llenarCampos('Edad')
+            return
+        }
+        if(correo.trim()===''){
+            llenarCampos('Correo')
+            return
+        }
+
+        const editado = lista.map(persona=>persona.codigo===codigo?{codigo,nombre,apellido,edad,correo}:persona)
+        setLista(editado)
+        setModoEdicion(false)
+
+        setNombre('')
+        setApellido('')
+        setEdad('')
+        setCorreo('')
+
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Datos Actualizados',
+            showConfirmButton: false,
+            timer: 1500
+            })
+    }
+    
+
+    const eliminar = (codigo) => { //2 parametros, el primero es el item y el segundo es el index
+
+  
         
         Swal.fire({
             title: 'Estas seguro de eliminar el registro?',
@@ -74,8 +129,11 @@ const Formulario = () => {
             }).then((result) => {
             
                 if (result.isConfirmed) {
-                const arrayFiltrado = lista.filter((item,indexLista)=>indexLista!==index)
-                setLista(arrayFiltrado)
+                const filtro = lista.filter((persona)=>persona.codigo!==codigo) //filtra el array y devuelve un nuevo array con los elementos que cumplan la condiciones de la funcion de callback
+                
+                
+                setLista(filtro)
+                debugger
                 Swal.fire(
                 'Eliminado!',
                 'El registro ha sido eliminado.',
@@ -86,46 +144,25 @@ const Formulario = () => {
     }
 
 
-    const editar = (item) => {
-        item.preventDefault() //evita que se recargue la pagina
-        if(!lista.trim()){
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'No hay datos para editar',
-                })
-                return
-        }
-
-        const arrayEditado = lista.map((item,index)=>(
-            {
-                nombre: nombre ? nombre : item.nombre,
-                apellido: apellido ? apellido : item.apellido,
-                edad: edad ? edad : item.edad,
-                correo: correo ? correo : item.correo
-
-                
-            }))
-            setLista(arrayEditado)
-            
-        }        
 
    
 
   return (
     <div className='container py-5'>
       <h1>Formulario</h1>
-        <form onSubmit={guardarDatos}>
+        <form className='form-group' >
             <input
                 type="text" 
                 placeholder='Nombre'
                 className='form-control mb-3'
+                value={nombre}
                 onChange={(e)=>{setNombre(e.target.value)}} //e.target.value es el valor que se ingresa en el input
                 />
             <input
                 type="text"
                 placeholder='Apellido'
                 className='form-control mb-3'
+                value={apellido}
                 onChange={(e)=>{setApellido(e.target.value)}}
                 />
             <input
@@ -134,16 +171,27 @@ const Formulario = () => {
                 max={100}
                 placeholder='Edad'
                 className='form-control mb-3'
+                value={edad}
                 onChange={(e)=>{setEdad(e.target.value)}}
                 />
             <input
                 type="email"
                 placeholder='Correo'
                 className='form-control mb-3'
+                value={correo}
                 onChange={(e)=>{setCorreo(e.target.value)}}
                 />
 
-            <button className='btn btn-primary' >Guardar Datos</button>
+              {
+                    modoEdicion ? 
+                    (<button className='btn btn-warning btn-block' 
+                      onClick={(e)=>{guardarCambios(e)}} type="submit">Guardar Cambios</button>):
+                    (<button className='btn btn-primary btn-block' 
+                    onClick={(e)=>{agregarUsuario(e)}}>Agregar Usuario</button>)
+
+              }      
+
+             
 
          </form>  
 
@@ -154,11 +202,11 @@ const Formulario = () => {
                         lista.length===0 ? (
                             <li className='list-group-item'>No hay personas Registradas..</li>
                         ) : (
-                            lista.map((item,index)=>( 
-                                <li className='list-group-item' key={index}>
-                                    {item.nombre} - {item.apellido} - {item.edad} - {item.correo} 
-                                    <button className='btn btn-danger' onClick={()=>{eliminar(index)}}>Eliminar</button> {''}
-                                     <button className='btn btn-warning' onClick={()=>{editar(item)}} >Editar</button>
+                            lista.map(persona=>( 
+                                <li className='list-group-item' key={codigo}>
+                                   {persona.codigo} -{persona.nombre} - {persona.apellido} - {persona.edad} - {persona.correo} 
+                                    <button className='btn btn-danger' onClick={()=>{eliminar(persona.codigo)}}>Eliminar</button> {''}
+                                     <button className='btn btn-warning' onClick={()=>{editar(persona)}}  >Editar</button>
                                      
                                 </li>
                             ))
